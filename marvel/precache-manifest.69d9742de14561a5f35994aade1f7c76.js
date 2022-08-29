1a52ddf7d881db3f2e57fe1a24e78996ecb2d515
Toindiana <76536112+Toindiana@users.noreply.github.com>
2022-08-29 10:54:40 +0300

    Add files via upload

---
+++ marvel/precache-manifest.69d9742de14561a5f35994aade1f7c76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9908a3b6e6ba6cf4ed19a25f0d2d0d6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b107563970f7a3bea12f",
+    "url": "/js/app.9ff9a320.js"
+  },
+  {
+    "revision": "1bb83a78973f5bddcc47",
+    "url": "/js/chunk-vendors.f3eb2517.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
